1424e4b057b0db47c95f4c849a4504692c0777dc
Boldeee <pasztor.boldi@gmail.com>
2021-12-01 10:20:40 +0100

    01_12 structolas

--- Nagybeadando/mainwindow.cpp
+++ Nagybeadando/mainwindow.cpp
@@ -1,15 +1,58 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    RGB szinek(0,0,0);
+    szin=QVector<QVector<RGB>>(3,QVector<RGB>(5,szinek));
+    //kiirat(szin);
 }
+struct  RGB
+{
+    int r,g,b;
+    RGB()
+    {
+        int r=0,g=0,b=0;
+    }
+    RGB(int _r,int _g, int _b)
+    {
+        r=_r;
+        g=_g;
+        b=_b;
+    }
+};
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+/*void kiirat(const QVector<QVector<RGB>>& szin)
+{
+    for(int i=0;i<szin.size();i++)
+    {
+        for(int j=0;j<5;j++)
+        {
+            qDebug()<<szin[i][j].b<<' ';
+        }
+    }
+}*/
+
+void MainWindow::on_pushButton_clicked()
+{
+    int XX=0;
+    for(int i=0;i<szin.size();i++)
+    {
+        for(int j=0;j<5;j++)
+        {
+            qDebug().nospace()<<'R'<<szin[i][j].r<<'G'<<szin[i][j].g<<'B'<<szin[i][j].b<<';';
+        }
+        XX++;
+        qDebug()<<XX;
+    }
 }
 
--- Nagybeadando/mainwindow.h
+++ Nagybeadando/mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QVector>
+#include <QDebug>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -12,8 +14,26 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
+    struct  RGB
+    {
+        int r,g,b;
+        RGB()
+        {
+            int r=0,g=0,b=0;
+        }
+        RGB(int _r,int _g, int _b)
+        {
+            r=_r;
+            g=_g;
+            b=_b;
+        }
+    };
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    QVector <QVector<RGB>> szin;
+
+private slots:
+    void on_pushButton_clicked();
 
 private:
     Ui::MainWindow *ui;
